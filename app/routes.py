cb9bd7a8602a4f83bea5e4fc0fbe3ef419e1b73f
nabilhassein <nabil.hassein@gmail.com>
2019-06-30 19:30:29 -0400

    temporarily working after import issues

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,64 @@
 import os
 from app import app
 from flask import render_template, request, redirect
+from flask_pymongo import PyMongo
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+app.config['MONGO_DBNAME'] = 'database'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:upperline2019@cluster0-n3alx.mongodb.net/database?retryWrites=true&w=majority'
+app.secret = b'insecure session verification'
 
+mongo = PyMongo(app)
 
-# from flask_pymongo import PyMongo
-
-# name of database
-# app.config['MONGO_DBNAME'] = 'database-name' 
-
-# URI of database
-# app.config['MONGO_URI'] = 'mongo-uri' 
-
-# mongo = PyMongo(app)
-
-
-# INDEX
 
 @app.route('/')
 @app.route('/index')
-
 def index():
+    collection = mongo.db.events
+    events = collection.find({})
     return render_template('index.html', events = events)
 
 
-# CONNECT TO DB, ADD DATA
-
 @app.route('/add')
-
 def add():
-    # connect to the database
+    events = mongo.db.events
 
-    # insert new data
+    events.insert({'event': 'homecoming', 'date': '2019-05-21'})
+    
+    return "added"
 
-    # return a message to the user
-    return ""
+
+@app.route('/events/new', methods=['GET', 'POST'])
+def new_event():
+    if request.method == "GET":
+        return render_template('new_event.html')
+
+    event_name = request.form['event_name']
+    event_date = request.form['event_date']
+    user_name = request.form['user_name']
+
+    events = mongo.db.events
+    events.insert({
+        'event': event_name,
+        'date': event_date,
+        'user': user_name
+    })
+    return redirect('/')
+
+
+# @app.route('/signup', methods=['POST', 'GET'])
+# def signup():
+#     if request.method == 'POST':
+#         users = mongo.db.users
+#         existing_user = users.find_one({'name' : request.form['username']})
+
+#         if existing_user is None:
+#             users.insert({
+#                 'name' : request.form['username'],
+#                 'password' : request.form['password']
+#             })
+#             session['username'] = request.form['username']
+#             return redirect(url_for('index'))
+
+#         return 'That username already exists! Try logging in.'
+
+#     return render_template('signup.html')
